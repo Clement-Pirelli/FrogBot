6afa172358bdd34532e5edcd8066df6ad53f5df1
Clement-pirelli <clepirelli@gmail.com>
2020-06-16 02:51:27 +0200

    Initial commit

---
+++ DiscordBot/Modules/Commands.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+using System.Linq;
+using Discord;
+using Discord.WebSocket;
+using System.IO;
+
+namespace DiscordBot.Modules
+{
+    class Commands : ModuleBase<SocketCommandContext>
+    {
+
+        private enum Minors
+        {
+            Programming, Graphics, Design, PM
+        }
+
+        private static readonly Dictionary<Minors, string> minorRoles = new Dictionary<Minors, string>()
+        {
+            { Minors.Programming, "Programming" },
+            { Minors.Graphics, "Graphics" },
+            { Minors.Design, "Design" },
+            { Minors.PM, "Project Management" }
+        };
+
+        private enum Years
+        {
+            Newcomer,
+            First,
+            Second,
+            Third,
+            Masters,
+            Alumni
+        }
+
+        private static readonly Dictionary<Years, string> yearRoles = new Dictionary<Years, string>()
+        {
+           {Years.Newcomer,"0 Year"},
+           {Years.First,"1st Year"},
+           {Years.Second,"2nd Year"},
+           {Years.Third,"3rd Year"},
+           {Years.Masters,"4th Year - Masters"},
+           {Years.Alumni,"Alumni"},
+        };
+
+
+        private bool IsValidRoleRequest(IGuildUser user, SocketRole role)
+        {
+            bool yearRole = yearRoles.ContainsValue(role.Name);
+            bool minorRole = minorRoles.ContainsValue(role.Name);
+            
+            if (!yearRole && !minorRole) return true;
+
+            foreach (var id in user.RoleIds)
+            {
+                if (id == role.Id) continue;
+
+                var roleName = Context.Guild.GetRole(id).Name;
+
+                if (yearRole)
+                {
+                    if (yearRoles.ContainsValue(roleName))
+                        return false;
+                }
+                else //minor role
+                {
+                    //masters students dont have minors
+                    if(roleName == yearRoles[Years.Masters] || minorRoles.ContainsValue(roleName))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+        private async Task SendChangeConfirmation()
+        {
+            if (Emote.TryParse("<:frogthumbsup:722037848890671105>", out var emote))
+            {
+                await Context.Message.AddReactionAsync(emote);
+            }
+            else
+            {
+                await Context.Message.AddReactionAsync(new Emoji("👍"));
+            }
+        }
+        private async Task SendChangeDenial() 
+        {
+            if (Emote.TryParse("<:frogthumbsdown:722234808327209010>", out var emote))
+            {
+                await Context.Message.AddReactionAsync(emote);
+            }
+            else
+            {
+                await Context.Message.AddReactionAsync(new Emoji("👎"));
+            }
+        }
+
+        private async Task ToggleRole(string roleName)
+        {
+            var user = Context.User as IGuildUser;
+            var ghostRole = Context.Guild.Roles.FirstOrDefault(x => x.Name == "Ghosts");
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == roleName);
+            
+            //check if the request is valid
+            if (ghostRole == null || role == null || !IsValidRoleRequest(user, role)) 
+            {
+                await SendChangeDenial();
+                return;
+            }
+            
+            //remove ghost role if the user has it
+            if (user.RoleIds.Contains(ghostRole.Id)) await user.RemoveRoleAsync(ghostRole);
+
+            if (user.RoleIds.Contains(role.Id))
+            {
+                Console.WriteLine($"Removed role {roleName} from user {user.Username + "#" + user.Discriminator}, id {user.Id}");
+                await user.RemoveRoleAsync(role);
+            } else
+            {
+                Console.WriteLine($"Added role {roleName} to user {user.Username + "#" + user.Discriminator}, id {user.Id}");
+                await user.AddRoleAsync(role);
+            }
+
+            await SendChangeConfirmation();
+        }
+
+        [Command("programming")]
+        public async Task Programmer() => await ToggleRole(minorRoles[Minors.Programming]);
+
+        [Command("graphics")]
+        public async Task Graphics() => await ToggleRole(minorRoles[Minors.Graphics]);
+
+        [Command("design")]
+        public async Task Design() => await ToggleRole(minorRoles[Minors.Design]);
+
+        [Command("pm")]
+        public async Task PM() => await ToggleRole(minorRoles[Minors.PM]);
+
+
+        [Command("newcomer")]
+        public async Task ZerothYear() => await ToggleRole(yearRoles[Years.Newcomer]);
+
+        [Command("1st")]
+        public async Task FirstYear() => await ToggleRole(yearRoles[Years.First]);
+
+        [Command("2nd")]
+        public async Task SecondYear() => await ToggleRole(yearRoles[Years.Second]);
+
+        [Command("3rd")]
+        public async Task ThirdYear() => await ToggleRole(yearRoles[Years.Third]);
+
+        [Command("4th")]
+        public async Task Masters() => await ToggleRole(yearRoles[Years.Masters]);
+
+        [Command("alumni")]
+        public async Task Alumni() => await ToggleRole(yearRoles[Years.Alumni]);
+
+        [Command("help")]
+        public async Task Help() 
+        {
+            await ReplyAsync(
+                "Frogbot to the rescue!\n" +
+                "All of my commands are preceded by \"!\"\n" +
+                "Minor role commands are : programming, graphics, design, pm\n" +
+                "Year role commands are: newcomer, 1st, 2nd, 3rd, 4th, alumni\n" +
+                "So far, that's about it!"
+                );
+        }
+
+        [Command("cleanup")]
+        public async Task Cleanup(int amount)
+        {
+            if (amount <= 0) amount = 100;
+
+            var user = Context.User as IGuildUser;
+            if (user.GuildPermissions.ManageMessages) 
+            {
+                var requestOptions = new RequestOptions();
+                requestOptions.AuditLogReason = $"Bot cleanup asked by {user.Nickname}#{user.Discriminator}";
+                var messagesRequest = Context.Channel.GetMessagesAsync(amount, options: requestOptions);
+                await foreach(var messages in messagesRequest) 
+                {
+                    foreach(var message in messages)
+                    {
+                        if (message.Content.StartsWith('!') || message.Author.IsBot)
+                        {
+                            await Context.Channel.DeleteMessageAsync(message);
+                            await Task.Delay(100);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DiscordBot/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Linq;
+using System.IO;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        public static readonly string logFileName = "DiscordBotLog.txt";
+        public static FileStream ostrm;
+        public static StreamWriter writer;
+        public static TextWriter stdOut;
+
+        public static readonly string welcomeMessage = "Welcome to the buddies server!\n" +
+                " Please read the rules, change your nickname to your real name and ask me for your roles!\n" +
+                "The current role commands are :\n" +
+                "   !programming:         for the programming minor\n" +
+                "   !graphics:            for the graphics minor\n" +
+                "   !pm:                  for the project management minor\n" +
+                "   !design:              for the design minor\n" +
+                "   !newcomer:            for people starting the education after the summer\n" +
+                "   !1st:                 for 1st year bachelors students\n" +
+                "   !2nd:                 for 2nd year bachelors students\n" +
+                "   !3rd:                 for 3rd year bachelors students\n" +
+                "   !4th:                 for 1st year masters students\n" +
+                "   !alumni:              for alumni\n" +
+                "You can also ask for !help if you don't remember a command!";
+
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client = new DiscordSocketClient();
+        private CommandService commands = new CommandService();
+        private IServiceProvider services;
+
+        public async Task RunBotAsync()
+        {
+            stdOut = Console.Out;
+            try
+            {
+                ostrm = new FileStream($"./{logFileName}", FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+                writer.AutoFlush = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Cannot open {logFileName} for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.WriteLine("Successfully opened log file. Starting discordbot...");
+            Console.SetOut(writer);
+            Console.WriteLine(DateTime.Now);
+
+
+
+            services = new ServiceCollection().AddSingleton(client).AddSingleton(commands).BuildServiceProvider();
+            string token = "NzIxNDk4NzYxODUxMDQzODkx.XuVaRA._UrbJ18j57wOwVcAmF6mKSOiCKA";
+
+            client.Log += ClientLog;
+
+            await RegisterCommandsAsync();
+
+            await client.LoginAsync(TokenType.Bot, token);
+
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task ClientLog(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandsAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            client.UserJoined += HandleUserJoinedAsync;
+            await commands.AddModuleAsync(typeof(Modules.Commands), services);
+        }
+
+        private async Task HandleUserJoinedAsync(SocketGuildUser arg)
+        {
+            if (arg.IsBot) return;
+            var ghostRole = arg.Guild.Roles.FirstOrDefault(x => x.Name == "Ghosts");
+            if (ghostRole != null) await arg.AddRoleAsync(ghostRole);
+
+            var generalChannel = client.GetChannel(441370070711533590) as SocketTextChannel;
+            await generalChannel.SendMessageAsync(welcomeMessage);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(client, message);
+            if (message.Author.IsBot) return;
+
+            int argPos = 0;
+            if(message.HasStringPrefix("!", ref argPos)) 
+            {
+                var result = await commands.ExecuteAsync(context, argPos, services);
+                if (!result.IsSuccess) 
+                {
+                    Console.WriteLine(result.ErrorReason);
+                }
+            } else
+            {
+                if (message.HasStringPrefix("frog ", ref argPos))
+                {
+                    var result = await commands.ExecuteAsync(context, argPos, services);
+                    if (!result.IsSuccess)
+                    {
+                        Console.WriteLine(result.ErrorReason);
+                    }
+                }
+            }
+        }
+    }
+}
